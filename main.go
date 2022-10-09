18263b48bf7d9bdcc0ee6a2f879257449fa9a361
dimosthenis <dimosthenis.manias@gmail.com>
2022-10-09 10:30:24 +0300

    mongo queries

--- main.go
+++ main.go
@@ -2,25 +2,40 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/dmanias/logs-audit/config"
 	"github.com/dmanias/logs-audit/mongo"
 	"github.com/hellofresh/health-go/v4"
 	log "github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson"
+	"net/http"
 	"os"
 	"strings"
 	"time"
 )
 
+type Event struct {
+	Timestamp time.Time              `json:"timestamp"`
+	Service   string                 `json:"service"`
+	EventType string                 `json:"eventType"`
+	Data      map[string]interface{} `json:"-"` // Rest of the fields should go here.
+}
+
 func main() {
 
-	//	jsons, err := loadJsons()
-	//	fmt.Println(jsons, err)
-	handleDB()
+	jsons, err := loadJsons()
+	if err != nil {
+		log.Fatal(err)
+	}
+	addToDB(jsons)
+
+	http.HandleFunc("/index/", queryDB)
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 func loadJsons() ([]string, error) {
 
-	filename := "jsons.txt"
+	filename := "json.txt"
 	content, err := os.ReadFile(filename)
 
 	if err != nil {
@@ -32,8 +47,7 @@ func loadJsons() ([]string, error) {
 	return split, nil
 }
 
-func handleDB() { //??
-	// set global log level
+func addToDB(jsons []string) {
 	cfg := config.New()
 	mongoClient, ctx, cancel, err := mongo.Connect(cfg.Database.Connector)
 	if err != nil {
@@ -56,7 +70,66 @@ func handleDB() { //??
 		log.Fatal(err)
 	}
 	db := mongoClient.Database("db")
-	events := db.Collection("events")
+	event := db.Collection("event")
 
-	fmt.Println("events results", events)
+	for _, jsonStr := range jsons {
+		json := jsonStruct(jsonStr)
+		_, err := event.InsertOne(ctx, bson.D{
+			{Key: "timestamp", Value: json.Timestamp},
+			{Key: "author", Value: "Nic Raboy"},
+			{Key: "tags", Value: bson.A{"development", "programming", "coding"}},
+		})
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	//	fmt.Println(eventResult, err)
 }
+
+func jsonStruct(jsonStr string) Event {
+	event := Event{}
+	if err := json.Unmarshal([]byte(jsonStr), &event); err != nil {
+		panic(err)
+	}
+	if err := json.Unmarshal([]byte(jsonStr), &event.Data); err != nil {
+		panic(err)
+	}
+	delete(event.Data, "timestamp")
+	delete(event.Data, "eventType")
+	delete(event.Data, "service")
+
+	fmt.Printf("%+v", event)
+
+	return event
+}
+
+func queryDB(w http.ResponseWriter, r *http.Request) {
+	// https://www.mongodb.com/blog/post/quick-start-golang--mongodb--how-to-read-documents
+	cfg := config.New()
+	mongoClient, ctx, cancel, err := mongo.Connect(cfg.Database.Connector)
+	if err != nil {
+		log.Fatal(err)
+		panic(err)
+	}
+
+	defer mongo.Close(mongoClient, ctx, cancel)
+
+	db := mongoClient.Database("db")
+	eventsCollection := db.Collection("event")
+
+	filterCursor, err := eventsCollection.Find(ctx, bson.M{"duration": 25})
+	if err != nil {
+		log.Fatal(err)
+	}
+	var eventsFiltered []bson.M
+	if err = filterCursor.All(ctx, &eventsFiltered); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(eventsFiltered)
+}
+
+//TODO JWT https://blog.logrocket.com/jwt-authentication-go/
+//TODO refactor connect to DB. add health check to the first function
+//TODO create function for errors
+//TODO template to show events
+//TODO probably replace MongoDB with elasticsearch
