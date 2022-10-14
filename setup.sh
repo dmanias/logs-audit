8c7c124255dec39c3071e2c38d9ea57de55b9c00
Dimosthenis Manias <dimosthenis.manias@gmail.om>
2022-10-14 13:05:17 +0300

    add run docker compose

--- setup.sh
+++ setup.sh
@@ -9,6 +9,9 @@ sudo apt install docker-ce
 mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
+sudo usermod -aG docker ${USER}
+su - ${USER}
+groups
 #install golang
 sudo apt install golang-go
 #install golang packages
