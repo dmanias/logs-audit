b2f9b40c28093ef162167dbd6198a5e7e2500bf2
Dimosthenis Manias <dimosthenis.manias@gmail.om>
2022-10-14 17:12:48 +0300

    remove su - ${USER} and groups

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ mkdir -p ~/.docker/cli-plugins/
 curl -SL https://github.com/docker/compose/releases/download/v2.3.3/docker-compose-linux-x86_64 -o ~/.docker/cli-plugins/docker-compose
 chmod +x ~/.docker/cli-plugins/docker-compose
 sudo usermod -aG docker ${USER}
+
 #install golang
 sudo apt install golang-go
 #Create Mongo directories
